a1b24da312cfd21ce3572afdf0c063b288f59c77
StephanLankhaar <38591947+stephanlankhaar@users.noreply.github.com>
2024-12-05 18:23:44 +0100

    shit's not working. To be continued

--- Dag 3/d3p2.py
+++ Dag 3/d3p2.py
@@ -4,19 +4,23 @@ results = 0
 with open("input.txt") as file:
     for row in file:
         multiply = re.findall('mul\([0-9]{1,3},[0-9]{1,3}\)|do\(\)|don\'t\(\)', row)
-        print("NEW LIST ----------")
-        processing = False
+        to_multiply=[]
+        processing = True
         for item in multiply:
             if item == 'do()':
                 processing = True
-            elif item == 'don\'t()':
+            if item == 'don\'t()':
                 processing = False
-            elif processing and item.startswith('mul('):
-                    number_1 = re.search(r"(?<=mul\()\d{1,3}", item).group()
-                    number_2 = re.search(r",(?<=,)\d{1,3}(?=\))", item).group()
-                    number_2 = number_2.replace(",","")
+            if processing:
+                if item != "do()":
+                    number_1 = re.search(r"(?<=mul\()\d{1,3}",
+                                         item).group()
+                    number_2 = re.search(r",(?<=,)\d{1,3}(?=\))",
+                                         item).group()
+                    number_2 = number_2.replace(",", "")
 
                     result = int(number_1) * int(number_2)
                     results += result
+
 
 print(results)
